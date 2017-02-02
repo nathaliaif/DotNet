246be972dec5bf6ea04855452f9aa7fe97e7a8b1
nathalia.iro.fernandes@gmail.com <logonrm@fiap.com.br>
2017-02-02 08:11:42 -0200

    Criando um Array de Time

---
+++ Array/Array/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Array
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnArray1 = new System.Windows.Forms.Button();
+            this.btnCarta = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnArray1
+            // 
+            this.btnArray1.Location = new System.Drawing.Point(43, 30);
+            this.btnArray1.Name = "btnArray1";
+            this.btnArray1.Size = new System.Drawing.Size(75, 23);
+            this.btnArray1.TabIndex = 0;
+            this.btnArray1.Text = "Array 1";
+            this.btnArray1.UseVisualStyleBackColor = true;
+            this.btnArray1.Click += new System.EventHandler(this.btnArray1_Click);
+            // 
+            // btnCarta
+            // 
+            this.btnCarta.Location = new System.Drawing.Point(138, 30);
+            this.btnCarta.Name = "btnCarta";
+            this.btnCarta.Size = new System.Drawing.Size(75, 23);
+            this.btnCarta.TabIndex = 1;
+            this.btnCarta.Text = "Carta";
+            this.btnCarta.UseVisualStyleBackColor = true;
+            this.btnCarta.Click += new System.EventHandler(this.btnCarta_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 340);
+            this.Controls.Add(this.btnCarta);
+            this.Controls.Add(this.btnArray1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnArray1;
+        private System.Windows.Forms.Button btnCarta;
+    }
+}
+
---
+++ Array/Array/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Array
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnArray1_Click(object sender, EventArgs e)
+        {
+            //Criando e iniciando um simples array
+            //Neste caso o tamanho do Array pode variar 
+            string[] times = {"Corinthians","Palmeiras","São Paulo", "Santos"};
+
+            //recuperando o conteúdo de um array
+            MessageBox.Show("Time" + times[0]);
+        }
+
+        private void btnCarta_Click(object sender, EventArgs e)
+        {
+            //Criando os naipes e tipos de cartas
+            string[] numeros = { "1","2","3","4","5","6","7","8","9","10","Valete","Dama","Rei"};
+
+            string[] naipe = { "Ouros", "Paus", "Espadas", "Copas" };
+
+            //Função para gerar números aleatórios
+            Random snumeros = new Random(12);
+            Random snaipes = new Random(3);
+
+            MessageBox.Show("Carta Sorteada: " + numeros[Convert.ToInt16(snumeros)] + " de " + naipe[Convert.ToInt16(snaipes)]);
+        }
+    }
+}
